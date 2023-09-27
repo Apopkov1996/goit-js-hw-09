96d7cab21161e32f87e864603e1cfbd6fcd0d1e5
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-09-27 09:56:04 +0000

    Deploying to gh-pages from @ Apopkov1996/goit-js-hw-09@373b34f1c5ace2e3e829e13e1cacf9f5ee7cdbac 🚀

---
+++ 01-color-switcher.ac61e954.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(t){t.currentTarget.setAttribute("disabled",!0),e.removeAttribute("disabled"),r=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(e){e.currentTarget.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(r)}));var r=null}();
+//# sourceMappingURL=01-color-switcher.ac61e954.js.map
