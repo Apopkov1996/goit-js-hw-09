373b34f1c5ace2e3e829e13e1cacf9f5ee7cdbac
Apopkov1996 <antonpopkov113@gmail.com>
2023-09-27 12:49:48 +0300

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+
+btnStartEl.addEventListener('click', onBtnStartClik);
+btnStopEl.addEventListener('click', oBtnStopClick);
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onBtnStartClik(e) {
+
+    e.currentTarget.setAttribute('disabled', true)
+    btnStopEl.removeAttribute('disabled')
+
+     intervalId = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor(); }, 1000);
+};
+
+
+function oBtnStopClick(e) {
+    e.currentTarget.setAttribute('disabled', true)
+    btnStartEl.removeAttribute('disabled')
+    clearInterval(intervalId)
+
+};
 
